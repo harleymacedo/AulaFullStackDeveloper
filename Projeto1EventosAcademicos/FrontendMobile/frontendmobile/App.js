36afbe1649bcf9e2308f78f67c87d315c97e2493
harleymacedo <harley.ip@gmail.com>
2023-05-22 14:16:15 -0300

    Apliativo mobile iniciado

---
+++ Projeto1EventosAcademicos/FrontendMobile/frontendmobile/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text1}>Eventos do IFCE Campus Crato</Text>
+      <Text style={styles.text2}>Próximos Eventos</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#282c34',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text1: {
+    color: '#fff',
+    fontSize: 20
+  },
+  text2: {
+    color: '#fff',
+    fontSize: 14,
+    marginTop: 20
+  }
+});
---
+++ Projeto1EventosAcademicos/FrontendWeb/frontendweb/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Eventos do IFCE Campus Crato</p>
+                <p>Próximos Eventos</p>
+            </header>
+        </div>
+    )
+}
+
+export default App;
